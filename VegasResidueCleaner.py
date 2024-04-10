74051e04f6b20a5b02b0898616d6cb29050dc427
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-04-10 09:56:44 +0200

    Added if else, incase nothing was deleted

--- VegasResidueCleaner.py
+++ VegasResidueCleaner.py
@@ -1,6 +1,7 @@
 import os
 
 def VegasResidueCleaner(rootFolder):
+    x = 0
     for root, dirs, files in os.walk(rootFolder):
         for file in files:
             if file.endswith(".mp4"):
@@ -8,14 +9,18 @@ def VegasResidueCleaner(rootFolder):
                 sfk_file_path = os.path.join(root, file + ".sfk")
                 
                 if os.path.exists(sfk_file_path):
+                    x+=x+1
                     print("\n")
                     os.remove(mp4_file_path)
                     print(f"{file} has been deleted.")
                     
                     os.remove(sfk_file_path)
                     print(f"{file}.sfk has been deleted.")
+    if (x != 0):
+        print("MP4 files and corresponding MP4.SFK files deleted successfully!")
+    else:
+        print("No files were deleted.")
 
-    print("MP4 files and corresponding MP4.SFK files deleted successfully!")
 
 # Define your folder
 rootFolder = "C:/Videos"
